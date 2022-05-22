262d13710aaaf86f060791049811f3316627bd22
Liam O'Donnell <spikything@gmail.com>
2022-05-22 18:29:30 +0100

    Adding orbit camera controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Canvas, Euler, MeshProps, useFrame } from '@react-three/fiber';
-import { ReactElement, useRef } from 'react';
+import { ReactElement, useRef, useState } from 'react';
+import { OrbitControls } from '@react-three/drei'
 
 function Box (rotation?:Euler):ReactElement {
 
   const box:MeshProps = useRef();
 
   useFrame((state, delta) => (
-    box.current.rotation.x += delta
-  ))
+    box.current.rotation.x += delta * 0.1,
+    box.current.rotation.y += delta * 0.05
+  ));
 
   return (
     <mesh ref={box} rotation={rotation?.rotation}>
@@ -20,9 +22,10 @@ function Box (rotation?:Euler):ReactElement {
 function App() {
 
   return <Canvas>
+    <OrbitControls />
     <ambientLight intensity={0.5} />
     <spotLight position={[10,10,10]} />
-    <Box rotation={[9,9,9] as Euler} />
+    <Box rotation={[9,9,9] as Euler}  />
   </Canvas>
 }
 
