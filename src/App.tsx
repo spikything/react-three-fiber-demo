dd08375e07a93cfc142a3431ae56767e84a3a1dc
Liam O'Donnell <spikything@gmail.com>
2022-05-30 18:08:23 +0100

    Added shadow casting

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,49 @@
 import { Canvas, Euler, MeshProps, useFrame } from '@react-three/fiber';
-import { ReactElement, useRef, useState } from 'react';
+import { ReactElement, useRef } from 'react';
 import { OrbitControls } from '@react-three/drei'
 
-function Box (rotation?:Euler):ReactElement {
+function Box ({rotation} : {rotation:Euler}):ReactElement {
 
   const box:MeshProps = useRef();
 
   useFrame((state, delta) => (
-    box.current.rotation.x += delta * 0.1,
-    box.current.rotation.y += delta * 0.05
+    box.current.rotation.x += delta * 0.2,
+    box.current.rotation.y += delta * 0.1
   ));
 
   return (
-    <mesh ref={box} rotation={rotation?.rotation}>
+    <mesh ref={box} position={[Math.random()*4-2, Math.random()*4-2, Math.random()*4-2]} rotation={rotation} castShadow receiveShadow>
       <boxBufferGeometry attach="geometry" />
-      <meshStandardMaterial attach="material" color="red" />
+      <meshStandardMaterial attach="material" color="red" transparent roughness={0.1} metalness={0.05} />
     </mesh>
   )
 }
 
 function App() {
 
-  return <Canvas>
+  return <Canvas shadows>
+
     <OrbitControls />
-    <ambientLight intensity={0.5} />
-    <spotLight position={[10,10,10]} />
+
+    <ambientLight intensity={0.25} castShadow />
+    <spotLight position={[20, 20, 20]} color='yellow' castShadow />
+    <spotLight position={[-20, -20, 20]} color='white' castShadow />
+
     <Box rotation={[9,9,9] as Euler}  />
+    <Box rotation={[6,6,6] as Euler}  />
+    <Box rotation={[30,0,0] as Euler}  />
+    <Box rotation={[1,2,3] as Euler}  />
+    <Box rotation={[0,90,0] as Euler}  />
+    <Box rotation={[0,0,0] as Euler}  />
+    <Box rotation={[0,0,0] as Euler}  />
+    <Box rotation={[0,0,0] as Euler}  />
+    <Box rotation={[-45,0,0] as Euler}  />
+    <Box rotation={[15,0,0] as Euler}  />
+    <Box rotation={[0,15,0] as Euler}  />
+    <Box rotation={[0,0,0] as Euler}  />
+    <Box rotation={[0,0,35] as Euler}  />
+    <Box rotation={[0,0,0] as Euler}  />
+    
   </Canvas>
 }
 
